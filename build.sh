4aee0fa3c5359051ece4d1b90e14f4099a3bc200
LeandroPanozzo <leandropanozzo1@gmail.com>
2025-04-03 17:45:37 -0300

    creacion de categorias

--- build.sh
+++ build.sh
@@ -11,6 +11,20 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
+# Crear estados de publicación
+python -c "
+from diarioback.models import EstadoPublicacion
+# Create each state
+states = [
+    ('borrador', 'Borrador'),
+    ('en_papelera', 'En Papelera'),
+    ('publicado', 'Publicado'),
+    ('listo_para_editar', 'Listo para editar'),
+]
+for code, name in states:
+    EstadoPublicacion.objects.get_or_create(nombre_estado=code)
+"
+
 #creacion de usuario admin 
 #export DJANGO_SUPERUSER_USERNAME=admin
 #export DJANGO_SUPERUSER_EMAIL=test@test.com
