90cf24822d2ff82b0a86740ca5a4ee4df101e615
LeandroPanozzo <leandropanozzo1@gmail.com>
2025-04-04 20:22:12 -0300

    creacion de estados de noticias

--- diario_back_api/urls.py
+++ diario_back_api/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -27,5 +30,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('diarioback/', include('diarioback.urls')),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
