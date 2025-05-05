04e10d42d7f8c31d65c5180a31ea6debf9076175
LeandroPanozzo <leandropanozzo1@gmail.com>
2025-05-05 20:44:57 -0300

    cambios en editores

--- diario_back_api/asgi.py
+++ diario_back_api/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diario_back_api.settingsProd')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diario_back_api.settings')
 
 application = get_asgi_application()
